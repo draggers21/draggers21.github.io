b22da409f25c1f59534eeb6cc91dfa784fbd639e
Ck1998 <ckunndra@gmail.com>
2021-07-10 08:31:13 +0530

    reduced prism js and css file sizes and changed core css

--- static/core/js/loader.js
+++ static/core/js/loader.js
@@ -3,7 +3,5 @@ setTimeout(function(){
     setTimeout(function(){
       $("#full-page-loader").removeClass("animated fadeOut");
       $("#full-page-loader").css("display","none");
-      window.location.hash = '#main-page-content';
-      window.location.hash = '';
     },500);
 },1000);
